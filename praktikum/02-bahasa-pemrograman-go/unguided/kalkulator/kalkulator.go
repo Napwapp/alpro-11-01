5e5bf0ed5d7f379566545bcbc6f89dcd63ed26d8
Napwapp <abduhnawaf@gmail.com>
2026-09-24 09:18:00 +0700

    tugas minggu2 praktikum alpro (guided dan mandiri) sebelumnya ada yang salah

--- praktikum/02-bahasa-pemrograman-go/unguided/kalkulator/kalkulator.go
+++ praktikum/02-bahasa-pemrograman-go/unguided/kalkulator/kalkulator.go
@@ -2,29 +2,32 @@ package main
 
 import "fmt"
 
-func main () {
+func main() {
 	var (
 		a, b int
 	)
 
-	// Input 
+	// Input
 	fmt.Println("Masukkan 2 angka untuk menampilkan hasil operasi dari kedua angka tersebut (+, -, *, /, %) ")
 	fmt.Println("Masukkan angka pertama: ")
 	fmt.Scanln(&a)
-	fmt.Println("\nMasukkan angka kedua: ")
-	fmt.Scanln(&b)
-	if b == 0 {
-		fmt.Println("Angka kedua tidak boleh 0. Masukkan lagi: ")
-		fmt.Scanln(&b)
-	}
 
-	// Output menggunakan formatting 
-	// % merupakan placeholder yang akan menampilkan nilai dari argumen setelahnya sesuai urutan 
+	for b == 0 {
+		fmt.Println("\nMasukkan angka kedua: ")
+		fmt.Scanln(&b)
+
+		if b == 0 {
+			fmt.Println("Angka kedua tidak boleh 0. Silahkan coba lagi! ")
+		}
+	}
+	
+	// Output menggunakan formatting
+	// % merupakan placeholder yang akan menampilkan nilai dari argumen setelahnya sesuai urutan
 	// d merupakan verb integer yang
 	fmt.Println("Hasil :")
-	fmt.Printf("\n%d + %d = %d\n", a, b, a + b)
-	fmt.Printf("%d - %d = %d\n", a, b, a - b)
-	fmt.Printf("%d * %d = %d\n", a, b, a * b)
-	fmt.Printf("%d / %d = %d\n", a, b, a / b)
-	fmt.Printf("%d %% %d = %d\n", a, b, a % b)
+	fmt.Printf("\n%d + %d = %d\n", a, b, a+b)
+	fmt.Printf("%d - %d = %d\n", a, b, a-b)
+	fmt.Printf("%d * %d = %d\n", a, b, a*b)
+	fmt.Printf("%d / %d = %d\n", a, b, a/b)
+	fmt.Printf("%d %% %d = %d\n", a, b, a%b)	
 }
